2be8140a8c089c1a824f9bf4e22ebf09c9c03e0f
Nicolas Denoël <nicolas.denoel@gmail.com>
2020-07-20 09:48:29 +0200

    Structure

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// SIMON : function 'Clicker' 
+// CALVIN : function 'Score'
+// ALBERT : function 'Multiplier'
+// NICO : layout
+// Variables : $score $counter $click $multiplier $upgradePrice $cookiesBySecond
